031ea48dc219dd997ba85dd6fc9310fdd9573917
Anna Schroeter <amschroeter@online.de>
2019-11-13 14:29:50 +0100

    rename functions, try udp

--- master/master.py
+++ master/master.py
@@ -7,30 +7,28 @@ hosts = ['127.0.0.1', '127.0.0.1']
 specs_list = [{"keyword": "rabbit", "website": "nature.com", "clientno": "client21"},{"keyword": "mouse", "website": "science.com", "clientno": "client21"}]
 port = 65432
 
-
-
-
 # This created a lot of troubles because of the string referencing
 # We solved it buy creating a new dictionary from scratch instead of assigning it to a new name
 def format_command(command, specs):
-    my_new_dict = {}
+    args_specific = {}
     for key in command[1]:
-        my_new_dict[key] = command[1][key].format(**specs)
-    return my_new_dict
+        args_specific[key] = command[1][key].format(**specs)
+    formatted_command = [command[0], args_specific]
+    return formatted_command
 
 
 # We are working on this function
-def stream_command(command, machines, machinespecs = None):
+def dist_command_tcp(command, machines, machinespecs = None):
     """
     machines: list of machines
     """
 
-    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     for no, mach in enumerate(machines):
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if machinespecs:
             command_to_run = format_command(command, machinespecs[no])
-            print(command)
-            print(command_to_run)
+            print("Target:" + mach + " Command:" + str(command_to_run), " \n")
         else:
             command_to_run = command
 
@@ -38,8 +36,28 @@ def stream_command(command, machines, machinespecs = None):
         data_to_send = command_to_run
         data = pickle.dumps(data_to_send)
         conn.sendall(data)
+        conn.close()
 
-    conn.close()
+
+def dist_command_udp(command, machines, machinespecs = None):
+    """
+    machines: list of machines
+    """
+
+
+    for no, mach in enumerate(machines):
+        conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        if machinespecs:
+            command_to_run = format_command(command, machinespecs[no])
+            print("Target:" + mach + " Command:" + str(command_to_run), " \n")
+        else:
+            command_to_run = command
+
+        conn.connect((mach, port))
+        data_to_send = command_to_run
+        data = pickle.dumps(data_to_send)
+        conn.sendall(data)
+        conn.close()
 
 
 
@@ -48,7 +66,7 @@ zleaf_command = ["run_zleaf", {"clientno": "{clientno}"}]
 url_command = ["open_url", {"url": "https://www.google.com/search?q={keyword}&as_sitesearch={website}"}]
 stream_command(url_command,hosts, machinespecs = specs_list)
 
-
+dist_command_udp(url_command,hosts, machinespecs = specs_list)
 
 
 myspecs
